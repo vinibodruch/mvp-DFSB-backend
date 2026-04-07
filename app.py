599945be7f8eaf34cde1c4b7b75cf571102a81b5
Vinícius Bodruch dos Santos <viniciusbodruch2009@hotmail.com>
2026-04-06 23:36:09 -0300

    feat: first structure app

--- app.py
+++ app.py
@@ -1,24 +1,35 @@
 from flask_cors import CORS
 from flask_openapi3 import Info, OpenAPI
+
+from src.models.database import init_db
 from src.routes.task_routes import bp as tasks_bp
 
 info = Info(
-    title="Todo API",
+    title="To-Do List API",
     version="1.0.0",
-    description="Aplicação de exemplo para gerenciamento de tarefas"
+    description=(
+        "Aplicação de gerenciamento de tarefas, permitindo criar, ler, atualizar e excluir tarefas."
+    ),
 )
 
 app = OpenAPI(__name__, info=info)
-CORS(app)
+CORS(app,
+    resources={r"/*": {"origins": "*"}}, # Permitir CORS para todas as origens
+    methods=["GET", "POST", "PATCH", "DELETE", "OPTIONS"] # Permitir os métodos HTTP usados na API
+)
+
 app.register_api(tasks_bp)
 
-@app.get("/health", responses={"200": {"description": "Aplicação está saudável"}})
+
+@app.get("/health", tags=[], responses={200: {"description": "API is running"}})
 def health():
-    """
-    Rota de verificação de saúde da aplicação.
-    """
+    """Endpoint de saúde para verificar se a API está funcionando."""
     return {"status": "ok"}, 200
 
 
+with app.app_context():
+    init_db()
+
+
 if __name__ == "__main__":
-    app.run(port=8080)
+    app.run(debug=True, port=8080)
